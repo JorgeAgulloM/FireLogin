1f6391392dd61d17d004fb563be8327d6d71412c
Jorge1 <agullojorge@gmail.com>
2024-03-28 12:00:21 +0100

    Implemented provisional login and navTo...

--- app/src/main/java/com/softyorch/firelogin/ui/login/LoginActivity.kt
+++ app/src/main/java/com/softyorch/firelogin/ui/login/LoginActivity.kt
@@ -1,10 +1,18 @@
 package com.softyorch.firelogin.ui.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.softyorch.firelogin.databinding.ActivityLoginBinding
+import com.softyorch.firelogin.ui.detail.DetailActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
@@ -16,5 +24,37 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI() {
+        initListeners()
+        initUiState()
+    }
+
+    private fun initUiState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.isLoading.collect { show ->
+                    binding.pbLoading.isVisible = show
+                }
+            }
+        }
+    }
+
+    private fun initListeners() {
+        binding.btnLogin.setOnClickListener {
+            viewModel.login(
+                user = binding.tieUser.text.toString(),
+                pass = binding.tiePass.text.toString()
+            ) {
+                navigateToDetail()
+            }
+        }
+    }
+
+    private fun navigateToDetail() {
+        Log.i("FireLoginTag", "NavigateToDetail")
+        startActivity(Intent(this, DetailActivity::class.java))
     }
 }
