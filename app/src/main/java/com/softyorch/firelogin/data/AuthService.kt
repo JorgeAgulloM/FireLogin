edcd95c94a98afed706b6a0dde409197093a026f
Jorge1 <agullojorge@gmail.com>
2024-03-28 19:06:56 +0100

    Implemented signUp and suspendCancelableCoroutine

--- app/src/main/java/com/softyorch/firelogin/data/AuthService.kt
+++ app/src/main/java/com/softyorch/firelogin/data/AuthService.kt
@@ -1,18 +1,34 @@
 package com.softyorch.firelogin.data
 
-import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.suspendCancellableCoroutine
 import javax.inject.Inject
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 
 class AuthService @Inject constructor(private val firebaseAuth: FirebaseAuth) {
 
     suspend fun login(user: String, pass: String): FirebaseUser? =
-        firebaseAuth.signInWithEmailAndPassword(user, pass).addOnFailureListener {
-            Log.e("FireLoginTag", "Error in login!!!! ${it.message}")
-        }.addOnCompleteListener {
-            Log.i("FireLoginTag", "Success login!!!!")
-        }.await().user
+        suspendCancellableCoroutine { cancellableContinuation ->
+            firebaseAuth.signInWithEmailAndPassword(user, pass)
+                .addOnSuccessListener {
+                    cancellableContinuation.resume(it.user)
+                }.addOnFailureListener { except ->
+                    cancellableContinuation.resumeWithException(except)
+                }
+        }
+
+
+    suspend fun signUp(email: String, pass: String): FirebaseUser? =
+        suspendCancellableCoroutine { cancellableContinuation ->
+            firebaseAuth.createUserWithEmailAndPassword(email, pass)
+                .addOnSuccessListener {
+                    cancellableContinuation.resume(it.user)
+                }
+                .addOnFailureListener { except ->
+                    cancellableContinuation.resumeWithException(except)
+                }
+        }
 
 }
