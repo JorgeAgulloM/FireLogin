2a1d367ecb910e2e1f041950a6f9fe71b11f8dde
Jorge1 <agullojorge@gmail.com>
2024-03-28 09:06:09 +0100

    Renamed MainActivity by SplashActivity

--- app/src/main/java/com/softyorch/firelogin/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/softyorch/firelogin/ui/splash/SplashActivity.kt
@@ -1,13 +1,16 @@
-package com.softyorch.firelogin
+package com.softyorch.firelogin.ui.splash
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.softyorch.firelogin.R
 import dagger.hilt.android.AndroidEntryPoint
 
+@SuppressLint("CustomSplashScreen")
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
