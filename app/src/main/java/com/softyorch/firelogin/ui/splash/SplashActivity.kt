f73df2928da1462a4fae11f8d79d70813f2089b2
Jorge1 <agullojorge@gmail.com>
2024-03-28 09:11:36 +0100

    Implemented binding and SplashViewModel

--- app/src/main/java/com/softyorch/firelogin/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/softyorch/firelogin/ui/splash/SplashActivity.kt
@@ -3,14 +3,21 @@ package com.softyorch.firelogin.ui.splash
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.softyorch.firelogin.R
+import com.softyorch.firelogin.databinding.ActivitySplashBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @SuppressLint("CustomSplashScreen")
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
+
+    private val splashViewModel: SplashViewModel by viewModels()
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
