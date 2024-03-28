02bde615ecfe6fc8d6475d0d2f66d96cff9f900d
Jorge1 <agullojorge@gmail.com>
2024-03-28 09:15:38 +0100

    Created and implement SignUpActivity with viewModel and binding

---
+++ app/src/main/java/com/softyorch/firelogin/ui/signup/SignUpActivity.kt
@@ -0,0 +1,20 @@
+package com.softyorch.firelogin.ui.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import com.softyorch.firelogin.R
+import com.softyorch.firelogin.databinding.ActivitySignUpBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SignUpActivity : AppCompatActivity() {
+
+    private val viewModel: SignUpViewModel by viewModels()
+    private lateinit var binding: ActivitySignUpBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
