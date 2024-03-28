ca75ddba88738b751099da7614217ce7278fac5c
Jorge1 <agullojorge@gmail.com>
2024-03-28 19:33:16 +0100

    Implemented logOut

--- app/src/main/java/com/softyorch/firelogin/ui/detail/DetailsViewModel.kt
+++ app/src/main/java/com/softyorch/firelogin/ui/detail/DetailsViewModel.kt
@@ -1,9 +1,19 @@
 package com.softyorch.firelogin.ui.detail
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.softyorch.firelogin.data.AuthService
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class DetailsViewModel @Inject constructor(): ViewModel() {
+class DetailsViewModel @Inject constructor(private val authService: AuthService) : ViewModel() {
+    fun logout(navigateToLogin: () -> Unit) {
+        viewModelScope.launch(Dispatchers.IO) {
+            authService.logout()
+        }
+        navigateToLogin()
+    }
 }
