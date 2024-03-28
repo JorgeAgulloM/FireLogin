169dbf40cf673cac91287b9f5c4cdebdada100d6
Jorge1 <agullojorge@gmail.com>
2024-03-28 19:08:11 +0100

    Implemented signUp

--- app/src/main/java/com/softyorch/firelogin/ui/signup/SignUpActivity.kt
+++ app/src/main/java/com/softyorch/firelogin/ui/signup/SignUpActivity.kt
@@ -1,10 +1,17 @@
 package com.softyorch.firelogin.ui.signup
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.softyorch.firelogin.databinding.ActivitySignUpBinding
+import com.softyorch.firelogin.ui.detail.DetailActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SignUpActivity : AppCompatActivity() {
@@ -15,5 +22,38 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
     }
+
+    private fun initUI() {
+        initListeners()
+        initUiState()
+    }
+
+    private fun initUiState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.isLoading.collect { show ->
+                    binding.pbLoading.isVisible = show
+                }
+            }
+        }
+    }
+
+    private fun initListeners() {
+        binding.btnSingUp.setOnClickListener {
+            viewModel.register(
+                email = binding.tieEmail.text.toString(),
+                pass = binding.tiePass.text.toString()
+            ) {
+                navigateToDetail()
+            }
+        }
+    }
+
+    private fun navigateToDetail() {
+        startActivity(Intent(this, DetailActivity::class.java))
+    }
+
+
 }
--- app/src/main/java/com/softyorch/firelogin/ui/signup/SignUpViewModel.kt
+++ app/src/main/java/com/softyorch/firelogin/ui/signup/SignUpViewModel.kt
@@ -1,9 +1,47 @@
 package com.softyorch.firelogin.ui.signup
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.softyorch.firelogin.data.AuthService
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
-class SignUpViewModel @Inject constructor(): ViewModel() {
+class SignUpViewModel @Inject constructor(private val authService: AuthService) : ViewModel() {
+
+
+    private var _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> = _isLoading
+
+    fun register(email: String, pass: String, navigateToDetail: () -> Unit) {
+        if (email.isNotEmpty() && pass.isNotEmpty()) {
+            viewModelScope.launch {
+                _isLoading.update { true }
+
+                try {
+                    val result = withContext(Dispatchers.IO) {
+                        authService.signUp(email, pass)
+                    }
+
+                    if (result != null) {
+                        navigateToDetail()
+                    } else {
+                        Log.w("FireLoginTag", "Firebase fail signUp")
+                    }
+
+                } catch (e: Exception) {
+                    Log.e("FireLoginTag", "Firebase error: ${e.message.orEmpty()}")
+                }
+                _isLoading.update { false }
+            }
+        }
+    }
+
 }
