617b5bfcf5586450a9c17a60fb0cf0f76feee88c
Jorge1 <agullojorge@gmail.com>
2024-03-28 09:20:16 +0100

    Created and implement DetailActivity with viewModel and binding

---
+++ app/src/main/java/com/softyorch/firelogin/ui/detail/DetailActivity.kt
@@ -0,0 +1,21 @@
+package com.softyorch.firelogin.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import com.softyorch.firelogin.R
+import com.softyorch.firelogin.databinding.ActivityLoginBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DetailActivity : AppCompatActivity() {
+
+    private val viewModel: DetailsViewModel by viewModels()
+    private lateinit var  binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
