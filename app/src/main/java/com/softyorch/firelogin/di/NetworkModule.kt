5fd528a5c463bf06e13a40c5da153115654b73eb
Jorge1 <agullojorge@gmail.com>
2024-03-28 11:58:48 +0100

    Created DI

---
+++ app/src/main/java/com/softyorch/firelogin/di/NetworkModule.kt
@@ -0,0 +1,17 @@
+package com.softyorch.firelogin.di
+
+import com.google.firebase.auth.FirebaseAuth
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideFirebaseAuth() = FirebaseAuth.getInstance()
+}
