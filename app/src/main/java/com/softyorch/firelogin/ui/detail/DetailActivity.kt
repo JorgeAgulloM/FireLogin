f967a6fad7ba7c417100db6738108cba68d46e35
Jorge1 <agullojorge@gmail.com>
2024-03-28 19:33:22 +0100

    Implemented logOut

--- app/src/main/java/com/softyorch/firelogin/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/softyorch/firelogin/ui/detail/DetailActivity.kt
@@ -1,9 +1,11 @@
 package com.softyorch.firelogin.ui.detail
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.softyorch.firelogin.databinding.ActivityDetailBinding
+import com.softyorch.firelogin.ui.login.LoginActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,5 +18,24 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI() {
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.btnLogout.setOnClickListener {
+            viewModel.logout {
+                navigateToLogin()
+            }
+        }
+    }
+
+    private fun navigateToLogin() {
+        startActivity(Intent(this, LoginActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        })
     }
 }
