5483074b913c724d2bf8bfe4d5c360d3bbcdc7b8
Jorge1 <agullojorge@gmail.com>
2024-03-28 11:59:16 +0100

    Implemented provisional navigation to login

--- app/src/main/java/com/softyorch/firelogin/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/softyorch/firelogin/ui/splash/SplashActivity.kt
@@ -1,10 +1,12 @@
 package com.softyorch.firelogin.ui.splash
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.softyorch.firelogin.databinding.ActivitySplashBinding
+import com.softyorch.firelogin.ui.login.LoginActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @SuppressLint("CustomSplashScreen")
@@ -18,5 +20,11 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        navigateToLogin()
     }
+
+    private fun navigateToLogin() {
+        startActivity(Intent(this, LoginActivity::class.java))
+    }
+
 }
