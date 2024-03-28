c406ec2f7eee039287492730dfcfd5b569c8d932
Jorge1 <agullojorge@gmail.com>
2024-03-28 19:24:11 +0100

    Implemented destination state

--- app/src/main/java/com/softyorch/firelogin/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/softyorch/firelogin/ui/splash/SplashViewModel.kt
@@ -1,9 +1,16 @@
 package com.softyorch.firelogin.ui.splash
 
 import androidx.lifecycle.ViewModel
+import com.softyorch.firelogin.data.AuthService
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class SplashViewModel @Inject constructor(): ViewModel() {
+class SplashViewModel @Inject constructor(private val authService: AuthService) : ViewModel() {
+
+    fun checkDestination(): SplashDestination = when (authService.isUserLogged()) {
+        true -> SplashDestination.Home
+        else -> SplashDestination.Login
+    }
+
 }
