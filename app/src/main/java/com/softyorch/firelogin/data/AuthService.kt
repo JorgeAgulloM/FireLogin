1768e1d1f24833c9bbdb94b62bcaf7da952375a7
Jorge1 <agullojorge@gmail.com>
2024-03-29 18:35:06 +0100

    Added gitHub login

--- app/src/main/java/com/softyorch/firelogin/data/AuthService.kt
+++ app/src/main/java/com/softyorch/firelogin/data/AuthService.kt
@@ -12,10 +12,12 @@ import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.OAuthProvider
 import com.google.firebase.auth.PhoneAuthOptions
 import com.google.firebase.auth.PhoneAuthProvider
 import com.softyorch.firelogin.R
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.CancellableContinuation
 import kotlinx.coroutines.suspendCancellableCoroutine
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
@@ -67,6 +69,20 @@ class AuthService @Inject constructor(
         return completeRegisterWithCredentials(credentials)
     }
 
+    suspend fun loginWithGitHub(activity: Activity): FirebaseUser? {
+        val provider = OAuthProvider.newBuilder("github.com").apply {
+            scopes = listOf("user:email")
+        }
+
+        return suspendCancellableCoroutine { cancellableContinuation ->
+            firebaseAuth.pendingAuthResult?.addOnSuccessListener { authResult ->
+                cancellableContinuation.resume(authResult.user)
+            }?.addOnFailureListener { ex ->
+                cancellableContinuation.resumeWithException(ex)
+            } ?: completeRegisterWithProvider(activity, provider.build(), cancellableContinuation)
+        }
+    }
+
     suspend fun signUp(email: String, pass: String): FirebaseUser? =
         suspendCancellableCoroutine { cancellableContinuation ->
             firebaseAuth.createUserWithEmailAndPassword(email, pass)
@@ -84,16 +100,6 @@ class AuthService @Inject constructor(
         LoginManager.getInstance().logOut()
     }
 
-    fun isUserLogged(): Boolean = getCurrentUser() != null
-
-    suspend fun verifyCode(verificationCode: String, phoneCode: String): FirebaseUser? {
-        val credentials = PhoneAuthProvider.getCredential(verificationCode, phoneCode)
-        return completeRegisterWithCredentials(credentials)
-    }
-
-    suspend fun completeRegisterWithPhoneVerification(credentials: AuthCredential): FirebaseUser? =
-        completeRegisterWithCredentials(credentials)
-
     fun getGoogleClient(): GoogleSignInClient {
         // cuando llamamos la primera vez a R.string.default_web_client_id no existe, pero al terminar el build y buildear la
         // app se autogenera dicho string
@@ -106,6 +112,16 @@ class AuthService @Inject constructor(
         return GoogleSignIn.getClient(context, gso)
     }
 
+    suspend fun completeRegisterWithPhoneVerification(credentials: AuthCredential): FirebaseUser? =
+        completeRegisterWithCredentials(credentials)
+
+    fun isUserLogged(): Boolean = getCurrentUser() != null
+
+    suspend fun verifyCode(verificationCode: String, phoneCode: String): FirebaseUser? {
+        val credentials = PhoneAuthProvider.getCredential(verificationCode, phoneCode)
+        return completeRegisterWithCredentials(credentials)
+    }
+
     private suspend fun completeRegisterWithCredentials(credentials: AuthCredential): FirebaseUser? =
         suspendCancellableCoroutine { cancellableContinuation ->
             firebaseAuth.signInWithCredential(credentials)
@@ -116,6 +132,19 @@ class AuthService @Inject constructor(
                     cancellableContinuation.resumeWithException(ex)
                 }
         }
+
+    private fun completeRegisterWithProvider(
+        activity: Activity,
+        provider: OAuthProvider,
+        cancellableContinuation: CancellableContinuation<FirebaseUser?>
+    ) {
+        firebaseAuth.startActivityForSignInWithProvider(activity, provider)
+            .addOnSuccessListener { authResume ->
+                cancellableContinuation.resume(authResume.user)
+            }.addOnFailureListener { ex ->
+                cancellableContinuation.resumeWithException(ex)
+            }
+    }
 
     private fun getCurrentUser() = firebaseAuth.currentUser
 
