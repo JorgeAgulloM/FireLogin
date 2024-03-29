0db60060a164d6560da7818e9b93a7516b85f884
Jorge1 <agullojorge@gmail.com>
2024-03-29 12:08:30 +0100

    Implemented login, verification & completeRegisterAuto with phone

--- app/src/main/java/com/softyorch/firelogin/data/AuthService.kt
+++ app/src/main/java/com/softyorch/firelogin/data/AuthService.kt
@@ -1,8 +1,13 @@
 package com.softyorch.firelogin.data
 
+import android.app.Activity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
 import kotlinx.coroutines.suspendCancellableCoroutine
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
@@ -19,6 +24,25 @@ class AuthService @Inject constructor(private val firebaseAuth: FirebaseAuth) {
                 }
         }
 
+    fun loginWithPhone(
+        phoneNumber: String,
+        activity: Activity,
+        callback: PhoneAuthProvider.OnVerificationStateChangedCallbacks
+    ) {
+
+        //Solo para pruebas
+        //firebaseAuth.firebaseAuthSettings.setAutoRetrievedSmsCodeForPhoneNumber("+34 123456789", "123456")
+
+        val options = PhoneAuthOptions
+            .newBuilder(firebaseAuth)
+            .setPhoneNumber(phoneNumber)
+            .setTimeout(60L, TimeUnit.SECONDS)
+            .setActivity(activity)
+            .setCallbacks(callback)
+            .build()
+
+        PhoneAuthProvider.verifyPhoneNumber(options)
+    }
 
     suspend fun signUp(email: String, pass: String): FirebaseUser? =
         suspendCancellableCoroutine { cancellableContinuation ->
@@ -36,6 +60,22 @@ class AuthService @Inject constructor(private val firebaseAuth: FirebaseAuth) {
     }
 
     fun isUserLogged(): Boolean = getCurrentUser() != null
+
+    suspend fun verifyCode(verificationCode: String, phoneCode: String): FirebaseUser? {
+        val credentials = PhoneAuthProvider.getCredential(verificationCode, phoneCode)
+        return completeRegisterWithPhone(credentials)
+    }
+
+    suspend fun completeRegisterWithPhone(credentials: PhoneAuthCredential): FirebaseUser? =
+        suspendCancellableCoroutine { cancellableContinuation ->
+            firebaseAuth.signInWithCredential(credentials)
+                .addOnSuccessListener { authResult ->
+                    cancellableContinuation.resume(authResult.user)
+                }
+                .addOnFailureListener { ex ->
+                    cancellableContinuation.resumeWithException(ex)
+                }
+        }
 
     private fun getCurrentUser() = firebaseAuth.currentUser
 
