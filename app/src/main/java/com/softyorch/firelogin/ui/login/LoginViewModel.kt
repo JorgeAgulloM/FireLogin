06632f34dbf725e332a52a443b250666a8760ea5
Jorge1 <agullojorge@gmail.com>
2024-03-29 12:09:17 +0100

    Implemented login, verification & completeRegisterAuto with phone & callback with state result

--- app/src/main/java/com/softyorch/firelogin/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/softyorch/firelogin/ui/login/LoginViewModel.kt
@@ -1,12 +1,17 @@
 package com.softyorch.firelogin.ui.login
 
+import android.app.Activity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
 import com.softyorch.firelogin.data.AuthService
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -16,6 +21,8 @@ class LoginViewModel @Inject constructor(private val authService: AuthService) :
 
     private var _isLoading = MutableStateFlow(false)
     val isLoading: StateFlow<Boolean> = _isLoading
+
+    private lateinit var verificationCode: String
 
     fun login(user: String, pass: String, navigateToDetails: () -> Unit) {
         if (user.isNotEmpty() && pass.isNotEmpty()) {
@@ -36,4 +43,58 @@ class LoginViewModel @Inject constructor(private val authService: AuthService) :
             }
         }
     }
+
+    fun verifyCode(phoneCode: String, onSuccessVerification: () -> Unit) {
+        viewModelScope.launch {
+            val result = withContext(Dispatchers.IO) {
+                authService.verifyCode(verificationCode, phoneCode)
+            }
+
+            if (result != null) {
+                onSuccessVerification()
+            }
+        }
+    }
+
+    fun loginWithPhone(phoneNumber: String, activity: Activity, onCallback: (PhoneVerification) -> Unit) {
+        if (phoneNumber.isNotEmpty()) viewModelScope.launch {
+            _isLoading.update { true }
+
+            val callback = onVerificationStateChangedCallbacks(onCallback)
+
+            withContext(Dispatchers.IO) {
+                authService.loginWithPhone(phoneNumber, activity, callback)
+            }
+
+            _isLoading.update { false }
+        }
+    }
+
+    private fun onVerificationStateChangedCallbacks(onCallback: (PhoneVerification) -> Unit) =
+        object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            override fun onVerificationCompleted(credentials: PhoneAuthCredential) {
+                viewModelScope.launch {
+
+                    val result = withContext(Dispatchers.IO) {
+                        authService.completeRegisterWithPhone(credentials)
+                    }
+
+                    if (result != null) {
+                        onCallback(PhoneVerification.VerifiedPhoneComplete)
+                    }
+                }
+            }
+
+            override fun onVerificationFailed(exception: FirebaseException) {
+                _isLoading.update { false }
+                onCallback(PhoneVerification.VerifiedPhoneFailure(exception.message.orEmpty()))
+            }
+
+            override fun onCodeSent(verificationCode: String, p1: PhoneAuthProvider.ForceResendingToken) {
+                _isLoading.update { false }
+                this@LoginViewModel.verificationCode = verificationCode
+                onCallback(PhoneVerification.CodeSend)
+            }
+        }
+
 }
