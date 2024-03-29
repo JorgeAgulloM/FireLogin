a470acd4815f838604c720836969e739d61bbd78
Jorge1 <agullojorge@gmail.com>
2024-03-29 13:58:49 +0100

    Implemented google auth library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.2.3"))
     implementation("com.google.firebase:firebase-analytics-ktx")
     implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
 
     // Dagger Hilt
     implementation ("com.google.dagger:hilt-android:2.45")
