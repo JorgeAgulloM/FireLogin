3e041394ccd962f3abaa7658028c7bb8921bb454
Jorge1 <agullojorge@gmail.com>
2024-03-29 11:56:43 +0100

    Added pin library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,9 @@ dependencies {
     // Activities
     implementation("androidx.activity:activity-ktx:1.7.2")
 
+    // Pin
+    implementation ("io.github.chaosleung:pinview:1.4.4")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
